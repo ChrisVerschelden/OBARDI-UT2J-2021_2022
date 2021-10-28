c719f35e1915c68ee9c96ba0fb16fce902dc7c80
Williams TRINH <trinh.williams084@gmail.com>
2021-10-28 09:22:44 +0200

    fix slider

--- PackageWill/Vis_Timeline/js/timeline_functions.js
+++ PackageWill/Vis_Timeline/js/timeline_functions.js
@@ -132,19 +132,23 @@ noUiSlider.create(slider, {
     }
 });
 
+slider.noUiSlider.on('slide',function(){
+  var min = slider.noUiSlider.get()[0];
+  var max = slider.noUiSlider.get()[1];
+  options['min'] = new Date(min, 1, 1);
+  options['max'] = new Date(max, 1, 1);
+  timeline.setOptions(options);
+  timeline.fit();
+})
+
 slider.noUiSlider.on('change',function(){
     var min = slider.noUiSlider.get()[0];
     var max = slider.noUiSlider.get()[1];
-    options['min'] = new Date(min, 1, 1);
-    options['max'] = new Date(max, 1, 1);
-    timeline.setOptions(options);
-    timeline.fit();
     tabVal2 = new Array;
     for(var val = min; val<=max; val = val + 10){
         tabVal2.push(val);
     }
     slider2.noUiSlider.updateOptions({
-        start: [min, max],
         range: {
             'min': min,
             'max': max
