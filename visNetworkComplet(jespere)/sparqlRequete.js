7bc425923d5c5ead264061bf378cc01ea20864d4
Chris Verschelden <chris.verschelden@outlook.fr>
2022-01-14 15:04:40 +0100

    tmp

---
+++ visNetworkComplet(jespere)/sparqlRequete.js
@@ -0,0 +1,189 @@
+let myMap = new Map();
+let myMapid = new Map();
+let groupe = new  Map();
+
+function load(data, group, level, recursif, idnem)
+{
+	let grouptemp = group;
+	let leveltemp = levelperms;
+	
+	if(level != 0)
+	{
+		leveltemp = level;
+	}
+	
+	for(let i = 0; i<data.length; i++)
+	{
+		if(group == 0)
+		{
+			grouptemp = groupperms;
+			groupperms = groupperms + 1;
+		}
+		
+		let trouve = false;
+		//Regarde si le nom de la node est déjà présent, si c'est le cas il ajoute ses enfants
+		for(let j = 0; j<nom.length; j++)
+		{
+			if(nom[j] == data[i].nom.value)
+			{
+				if(recursif && level != 1)
+				{
+					//Recupere le groupe de la node mère
+					let groupnem = grouptemp;
+					for (const key of myMap.keys()) {
+					  if(key.value == data[i].nom.value)
+					  {
+						  groupnem = myMap.get(key);
+					  }
+					}
+					
+					//Recupere l'id de la node mère
+					let idnem = id;
+					for (const key of myMapid.keys()) {
+					  if(key.value == data[i].nom.value)
+					  {
+						  idnem = myMapid.get(key);
+					  }
+					}
+
+					//Recupere la node enfants
+					retrieveNom(data[i].nomdeceluiquiestdessous.value, groupnem, (leveltemp + 1), idnem);
+				}
+				trouve = true;
+			}
+		}
+		
+		//Cas du nom de la nodes inexistant 
+		if(!trouve)
+		{
+			//Attribution d'une couleur pour un groupe
+			let trouvergroupe = false;
+			let color;
+
+			for (const key of groupe.keys())
+			{
+				if(data[i].nomgroupe.value == key)
+				{
+					trouvergroupe = true;
+					color = groupe.get(key)[0];
+				}
+			}
+			
+			
+			if(!trouvergroupe)
+			{
+				groupe.set(data[i].nomgroupe.value, [colorgroup[couleurcomp],leveltemp]);
+				color = colorgroup[couleurcomp];
+				couleurcomp = couleurcomp + 1;
+			}
+			
+			//Ajout de la node
+			if(leveltemp == 1)
+			{
+				nodes.add({id: id, label: data[i].nom.value, title: "TourLab",value: 30,group: grouptemp ,level: leveltemp, levelLabel: data[i].nomgroupe, hidden: false, expanded: false, color : color});
+			}
+			else
+			{
+				nodes.add({id: id, label: data[i].nom.value, title: "TourLab",value: 30,group: grouptemp ,level: leveltemp, levelLabel: data[i].nomgroupe, hidden: false, expanded: false, color : color});
+			}
+			nom.push(data[i].nom.value);
+			myMap.set(data[i].nom, grouptemp);
+			myMapid.set(data[i].nom, id);
+			
+			//Ajout des liaisons
+			if(idnem != 0)
+			{
+				edges.add({id: idedges,label: "subFeature", from: idnem , to: id , arrows:"to", hidden: false});
+				idedges = idedges + 1;
+			}
+			
+			id = id + 1;
+		}
+	}
+
+	updateReferencePoint();
+	updateLegend();
+}
+
+function retrieveDataSup() {
+  var query = "PREFIX : <http://www.semanticweb.org/lucas/ontologies/2021/11/HHT_Ontology#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?nom WHERE { ?x a :Area . ?x rdfs:label ?nom . MINUS { ?x a :Area . ?x :hasUpperUnit ?z . } }";
+  var url = 'http://localhost:7200/repositories/test?query=' + encodeURIComponent(query) + '&output=json';
+
+  $.ajax({
+	url: url,
+	dataType: "json",
+	success: function (data) {
+	  $('#results').show();
+	  $('#raw_output').text(JSON.stringify(data, null, 3));
+	  for(let i = 0; i<data.results.bindings.length; i++)
+	  {
+		  retrieveNom(data.results.bindings[i].nom.value, 0, 1, 0);
+	  }
+	  
+	},
+	error: function(e) {console.log("wesh ya pb la bro");}
+  });
+}
+
+function retrieveNom(name, group, level, idnem)
+{
+	  var query = "PREFIX : <http://www.semanticweb.org/lucas/ontologies/2021/11/HHT_Ontology#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?nom ?nomgroupe ?nomdeceluiquiestdessous  WHERE { ?x a :Area . ?x rdfs:label ?nom . ?x :isMemberOf ?groupe . ?groupe rdfs:label ?nomgroupe . ?x rdfs:label \""+name+"\" . ?x :hasSubUnit ?sousunite . ?sousunite rdfs:label ?nomdeceluiquiestdessous .}";
+
+	  var url = 'http://localhost:7200/repositories/test?query=' + encodeURIComponent(query) + '&output=json';
+
+	  $.ajax({
+		url: url,
+		dataType: "json",
+		success: function (data) {
+		  $('#results').show();
+		  $('#raw_output').text(JSON.stringify(data, null, 3));
+		  if(data.results.bindings.length == 0)
+		  {
+			  retrieveNomSansEnfants(name, group, level, idnem);
+		  }
+		  else
+		  {
+			  data = dataremovedoublons(data.results.bindings);
+			  load(data, group, level, true, idnem);
+		  }
+		},
+		error: function(e) {console.log("wesh ya pb la bro");}
+	  });
+}
+
+function retrieveNomSansEnfants(name, group, level, idnem)
+{	  
+	  var query = "PREFIX : <http://www.semanticweb.org/lucas/ontologies/2021/11/HHT_Ontology#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?nom ?nomgroupe WHERE { ?x a :Area . ?x rdfs:label ?nom . ?x :isMemberOf ?groupe . ?groupe rdfs:label ?nomgroupe . ?x rdfs:label \""+ name +"\" . }";
+	  var url = 'http://localhost:7200/repositories/test?query=' + encodeURIComponent(query) + '&output=json';
+
+	  $.ajax({
+		url: url,
+		dataType: "json",
+		success: function (data) {
+		  $('#results').show();
+		  $('#raw_output').text(JSON.stringify(data, null, 3, false));
+		  if(data.results.bindings.length == 0)
+		  {
+			  console.log("erreur");
+		  }
+		  else
+		  {
+			  load(data.results.bindings, group, level, false, idnem);
+		  }
+		},
+		error: function(e) {console.log("wesh ya pb la bro");}
+	  });
+}
+
+function dataremovedoublons(dataval)
+{
+	let val = [];
+	for(let i = 0; i<dataval.length; i++)
+	{
+		if(!(dataval[i].nomdeceluiquiestdessous.value == dataval[i].nom.value))
+		{
+			val.push(dataval[i]);
+		}
+	}
+	return val;
+}
--- visNetworkComplet(jespere)/vis.js
+++ visNetworkComplet(jespere)/vis.js
@@ -6,43 +6,17 @@ let nom = [];
 let couleurcomp = 0;
 let colorgroup = ["red","green","yellow","blue","pink","orange", "white"];
 
-
-function hide(element){
-	var id_node_origine = parseInt(element.id,10);
-
-	var connectedEdges = network.getConnectedEdges(id_node_origine, "from");
-
-	connectedEdges.forEach( e => {
-		var child_node = nodes.get(e);
-		var child_node_id = parseInt(child_node.id, 10);
-		if (network.getConnectedEdges(child_node_id, "from").length === 1) {
-			nodes.remove(child_node_id);
-		}
-		edges.remove(e);
-	});
-
-	for(let i = 0; i<nom.length; i++){
-		if(nom[i] == name){
-			delete nom[i];
-		}
-	}
-}	
-	
-var color = "gray";
-var len = undefined;
-
-var nodes = new vis.DataSet([]);
-var edges = new vis.DataSet([]);
-
+var setLevels = new Set();
+var referencePoints = new Set();
 
 // create a network
-var container = document.getElementById("mynetwork");
-
+var nodes = new vis.DataSet([]);
+var edges = new vis.DataSet([]);
 var data = {
 	nodes: nodes,
 	edges: edges,
 };
-
+var container = document.getElementById("mynetwork");
 var options = {
 	interaction: {
 		navigationButtons: true,
@@ -73,7 +47,6 @@ var options = {
 		},
 	},
 };
-
 network = new vis.Network(container, data, options);
 
 network.on("click", function(params) {
@@ -94,6 +67,27 @@ network.on("click", function(params) {
 	updateLegend();
 });
 
+function hide(element){
+	var id_node_origine = parseInt(element.id,10);
+	var connectedEdges = network.getConnectedEdges(id_node_origine, "from");
+
+	connectedEdges.forEach( e => {
+		var edge = edges.get(e)
+        var child_node_id = parseInt(edge.to,10);
+
+		var nomnode = nodes.get(child_node_id);
+		var name = nomnode.label;
+		for(let i = 0; i<nom.length; i++){
+			if(nom[i] == name){
+				delete nom[i];
+			}
+		}
+		if (network.getConnectedEdges(child_node_id, "from").length === 1) {
+			nodes.remove(child_node_id);
+		}
+		edges.remove(e);
+	});	
+}	
 
 function getPosDom(e) {
 	var pos = network.getPositions(e.id)[e.id];
@@ -118,8 +112,6 @@ function updateLegend(){
 	})
 }
 
-var setLevels = new Set();
-var referencePoints = new Set();
 function updateReferencePoint(){
 	setLevels = new Set();
 	referencePoints = new Set();
@@ -134,8 +126,15 @@ function updateReferencePoint(){
 function composeLegendElement(pos, e){
 	var obj = document.createElement('div');
 	obj.id = ""+e.id;
-	obj.innerText = e.label;
-	obj.style.cssText = 'position:absolute;z-index: 100;top:'+ pos.y + 'px;margin-left:10px;width:100px;height:50px;-moz-border-radius:10px;border:1px  solid #ddd;-moz-box-shadow: 0px 0px 8px  #fff';
+
+	for (const [key, value] of groupe.entries()) {
+		if(value[1] === e.level){
+			obj.innerHTML = '<p style="color:' + value[0] +  ';">' + key + '</p>';
+			//obj.innerText = '<p color="' +  +  '">' + e.label + '</p>';
+			obj.style.cssText = 'position:absolute;z-index: 100;top:'+ (pos.y + 50) + 'px;margin-left:10px;width:100px;height:50px;-moz-border-radius:10px;border:1px  solid #ddd;-moz-box-shadow: 0px 0px 8px  #fff';
+		}
+	}
+
 	obj.classList.add('legende');
 	return obj;
 }
@@ -166,196 +165,5 @@ document.addEventListener("mouseup", mouseup);
 //Also clear the interval when user leaves the window with mouse
 document.addEventListener("mouseout", mouseup);
 
-let myMap = new Map();
-let myMapid = new Map();
-let groupe = new  Map();
-
-function load(data, group, level, recursif, idnem)
-{
-	console.log(data);
-	let grouptemp = group;
-	let leveltemp = levelperms;
-	
-	if(level != 0)
-	{
-		leveltemp = level;
-	}
-	
-	for(let i = 0; i<data.length; i++)
-	{
-		if(group == 0)
-		{
-			grouptemp = groupperms;
-			groupperms = groupperms + 1;
-		}
-		
-		let trouve = false;
-		//Regarde si le nom de la node est déjà présent, si c'est le cas il ajoute ses enfants
-		for(let j = 0; j<nom.length; j++)
-		{
-			if(nom[j] == data[i].nom.value)
-			{
-				if(recursif && level != 1)
-				{
-					//Recupere le groupe de la node mère
-					let groupnem = grouptemp;
-					for (const key of myMap.keys()) {
-					  if(key.value == data[i].nom.value)
-					  {
-						  groupnem = myMap.get(key);
-					  }
-					}
-					
-					//Recupere l'id de la node mère
-					let idnem = id;
-					for (const key of myMapid.keys()) {
-					  if(key.value == data[i].nom.value)
-					  {
-						  idnem = myMapid.get(key);
-					  }
-					}
-
-					//Recupere la node enfants
-					retrieveNom(data[i].nomdeceluiquiestdessous.value, groupnem, (leveltemp + 1), idnem);
-				}
-				trouve = true;
-			}
-		}
-		
-		//Cas du nom de la nodes inexistant 
-		if(!trouve)
-		{
-			//Attribution d'une couleur pour un groupe
-			let trouvergroupe = false;
-			let color;
-
-			for (const key of groupe.keys())
-			{
-				if(data[i].nomgroupe.value == key)
-				{
-					trouvergroupe = true;
-					color = groupe.get(key)[0];
-				}
-			}
-			
-			
-			if(!trouvergroupe)
-			{
-				groupe.set(data[i].nomgroupe.value, [colorgroup[couleurcomp],leveltemp]);
-				color = colorgroup[couleurcomp];
-				couleurcomp = couleurcomp + 1;
-			}
-			
-			//Ajout de la node
-			if(leveltemp == 1)
-			{
-				nodes.add({id: id, label: data[i].nom.value, title: "TourLab",value: 30,group: grouptemp ,level: leveltemp, levelLabel: data[i].nomgroupe, hidden: false, expanded: false, color : color});
-			}
-			else
-			{
-				nodes.add({id: id, label: data[i].nom.value, title: "TourLab",value: 30,group: grouptemp ,level: leveltemp, levelLabel: data[i].nomgroupe, hidden: false, expanded: false, color : color});
-			}
-			nom.push(data[i].nom.value);
-			myMap.set(data[i].nom, grouptemp);
-			myMapid.set(data[i].nom, id);
-			
-			//Ajout des liaisons
-			if(idnem != 0)
-			{
-				edges.add({id: idedges,label: "subFeature", from: idnem , to: id , arrows:"to", hidden: false});
-				idedges = idedges + 1;
-			}
-			
-			id = id + 1;
-		}
-	}
-
-	updateReferencePoint();
-	updateLegend();
-}
-
-function retrieveDataSup() {
-  var query = "PREFIX : <http://www.semanticweb.org/lucas/ontologies/2021/11/HHT_Ontology#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?nom WHERE { ?x a :Area . ?x rdfs:label ?nom . MINUS { ?x a :Area . ?x :hasUpperUnit ?z . } }";
-  var url = 'http://localhost:7200/repositories/test?query=' + encodeURIComponent(query) + '&output=json';
-
-  $.ajax({
-	url: url,
-	dataType: "json",
-	success: function (data) {
-	  $('#results').show();
-	  $('#raw_output').text(JSON.stringify(data, null, 3));
-	  for(let i = 0; i<data.results.bindings.length; i++)
-	  {
-		  retrieveNom(data.results.bindings[i].nom.value, 0, 1, 0);
-	  }
-	  
-	},
-	error: function(e) {console.log("wesh ya pb la bro");}
-  });
-}
-
-function retrieveNom(name, group, level, idnem)
-{
-	  var query = "PREFIX : <http://www.semanticweb.org/lucas/ontologies/2021/11/HHT_Ontology#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?nom ?nomgroupe ?nomdeceluiquiestdessous  WHERE { ?x a :Area . ?x rdfs:label ?nom . ?x :isMemberOf ?groupe . ?groupe rdfs:label ?nomgroupe . ?x rdfs:label \""+name+"\" . ?x :hasSubUnit ?sousunite . ?sousunite rdfs:label ?nomdeceluiquiestdessous .}";
-
-	  var url = 'http://localhost:7200/repositories/test?query=' + encodeURIComponent(query) + '&output=json';
-
-	  $.ajax({
-		url: url,
-		dataType: "json",
-		success: function (data) {
-		  $('#results').show();
-		  $('#raw_output').text(JSON.stringify(data, null, 3));
-		  if(data.results.bindings.length == 0)
-		  {
-			  retrieveNomSansEnfants(name, group, level, idnem);
-		  }
-		  else
-		  {
-			  data = dataremovedoublons(data.results.bindings);
-			  load(data, group, level, true, idnem);
-		  }
-		},
-		error: function(e) {console.log("wesh ya pb la bro");}
-	  });
-}
-
-function retrieveNomSansEnfants(name, group, level, idnem)
-{	  
-	  var query = "PREFIX : <http://www.semanticweb.org/lucas/ontologies/2021/11/HHT_Ontology#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?nom ?nomgroupe WHERE { ?x a :Area . ?x rdfs:label ?nom . ?x :isMemberOf ?groupe . ?groupe rdfs:label ?nomgroupe . ?x rdfs:label \""+ name +"\" . }";
-	  var url = 'http://localhost:7200/repositories/test?query=' + encodeURIComponent(query) + '&output=json';
-
-	  $.ajax({
-		url: url,
-		dataType: "json",
-		success: function (data) {
-		  $('#results').show();
-		  $('#raw_output').text(JSON.stringify(data, null, 3, false));
-		  if(data.results.bindings.length == 0)
-		  {
-			  console.log("erreur");
-		  }
-		  else
-		  {
-			  load(data.results.bindings, group, level, false, idnem);
-		  }
-		},
-		error: function(e) {console.log("wesh ya pb la bro");}
-	  });
-}
-
-function dataremovedoublons(dataval)
-{
-	let val = [];
-	for(let i = 0; i<dataval.length; i++)
-	{
-		if(!(dataval[i].nomdeceluiquiestdessous.value == dataval[i].nom.value))
-		{
-			val.push(dataval[i]);
-		}
-	}
-	return val;
-}
-
+//charge les données du premier niveau
 retrieveDataSup();
-
