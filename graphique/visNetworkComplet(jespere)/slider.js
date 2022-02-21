259240299c5fd116cb11654b4ce8205fa65e5319
louannecostes <louanne.costes@gmail.com>
2022-02-21 14:50:56 +0100

    slider markers

--- graphique/visNetworkComplet(jespere)/slider.js
+++ graphique/visNetworkComplet(jespere)/slider.js
@@ -2,6 +2,13 @@ var anneeMin = 1660, anneeMax = 1800;
 
 var slider = document.getElementById("slider");
 
+var tabVal = new Array();
+var tabVal2 = new Array();
+
+for (var val = anneeMin; val <= anneeMax; val = val + 10) {
+    tabVal.push(val);
+    tabVal2.push(val);
+}
 noUiSlider.create(slider, {
     start: [anneeMin, anneeMax],
     connect: true,
@@ -19,6 +26,11 @@ noUiSlider.create(slider, {
             return value.replace();
         }
     },
+    pips: {
+        mode: 'values',
+        values: tabVal,
+        density: 2,
+    }
 });
 
 slider.noUiSlider.on('slide', function() {
