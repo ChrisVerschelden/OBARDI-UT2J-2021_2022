7a5240f52a212d9b7c9a5571dacbd7824772d762
cedricmithyx <57490437+cedricmithyx@users.noreply.github.com>
2022-03-09 16:16:09 +0100

    form avec query
    ajout des query pour les form

--- form.js
+++ form.js
@@ -1,36 +1,83 @@
-
-
 function testerRadio() {
-    var elements =[];
+    var elements = [];
     elements.push(document.getElementById("element"));
     elements.push(document.getElementById("niveau"));
 
     elements.forEach(e => {
-        if (e.checked && e.value === 'element'){
-            console.log(e.value);
+        if (e.checked && e.value === 'element') {
             document.getElementById('furtive').style.display = "block";
         } else if (e.checked) {
-            console.log("hey")
             document.getElementById('furtive').style.display = "none";
         }
-        
-    });  
-    
-  }
+
+    });
+
+}
 
 document.getElementById("niveau").addEventListener('click', () => {
-    console.log('yo');
     document.getElementById('furtive').style.display = "block";
     document.getElementById('nomUnite').style.display = "none";
-    document.getElementById('furtive').innerHTML = '<select name="niveau" id="niveau-select"><option value="generalites">Généralités</option><option value="elections">Elections</option><option value="paroisses">Paroisses</option></select>';
+    niveau();
 })
 
 document.getElementById("element").addEventListener('click', () => {
-    console.log('ya');
     document.getElementById('furtive').style.display = "block";
     document.getElementById('nomUnite').style.display = "block";
-
-    document.getElementById('furtive').innerHTML = ' <select name="element" id="element-select"><option value="e1">e1</option><option value="e2">e2</option><option value="e3">e3</option></select>';
+    elem();
 })
 
+
+namesearch = document.getElementById('firstname');
+namesearch.addEventListener('input', () => {
+    elem();
+})
+
+function elem(){
+    namesearch = document.getElementById('firstname');
+
+    text = '<select name="niveau" id="niveau-select">';
+
+    var query = "PREFIX : <http://www.semanticweb.org/lucas/ontologies/2021/11/HHT_Ontology#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?name ?nomgroupe WHERE { ?x a :Area  . ?x rdfs:label ?name . ?x :isMemberOf ?groupe . ?groupe rdfs:label ?nomgroupe . FILTER regex(?name, \"" + namesearch.value + "\", \"i\") }";
+    var url = 'http://localhost:7200/repositories/test?query=' + encodeURIComponent(query) + '&output=json';
+    console.log(query);
+    $.ajax({
+        url: url,
+        dataType: "json",
+        success: function (data) {
+            $('#results').show();
+            $('#raw_output').text(JSON.stringify(data, null, 3));
+            for (let i = 0; i < data.results.bindings.length; i++) {
+                text += "<option value=\"" + data.results.bindings[i].name.value + "\">" + data.results.bindings[i].name.value +" ("+data.results.bindings[i].nomgroupe.value+")" + "</option>";
+            }
+            text += '</select>';
+            document.getElementById('furtive').innerHTML = text;
+
+        },
+        error: function (e) { console.log("Query error"); }
+    });
+}
+
+function niveau(){
+    text = '<select name="niveau" id="niveau-select">';
+
+    var query = "PREFIX : <http://www.semanticweb.org/lucas/ontologies/2021/11/HHT_Ontology#>PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>select ?name where {?x a :LevelVersion .?x rdfs:label ?name .}";
+    var url = 'http://localhost:7200/repositories/test?query=' + encodeURIComponent(query) + '&output=json';
+    console.log(query);
+    $.ajax({
+        url: url,
+        dataType: "json",
+        success: function (data) {
+            $('#results').show();
+            $('#raw_output').text(JSON.stringify(data, null, 3));
+            console.log(data);
+            for (let i = 0; i < data.results.bindings.length; i++) {
+                text += "<option value=\"" + data.results.bindings[i].name.value + "\">" + data.results.bindings[i].name.value +"</option>";
+            }
+            text += '</select>';
+            document.getElementById('furtive').innerHTML = text;
+
+        },
+        error: function (e) { console.log("Query error"); }
+    });
+}
 
