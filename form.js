0aa5cbfbca9f55aa61e5bd14086477d75f00ff44
cedricmithyx <57490437+cedricmithyx@users.noreply.github.com>
2022-03-10 14:56:44 +0100

    update formulaire
    update du formulaire

--- form.js
+++ form.js
@@ -39,35 +39,29 @@ document.getElementById("element").addEventListener('click', () => {
 
 namesearch = document.getElementById('firstname');
 namesearch.addEventListener('input', () => {
-    elem();
+    elem(true);
 })
 
 function getValue(selectObject) {
     var str = selectObject.value;
-    var mySubString = str;
-    var mySubString2 = str;
-    if(str.includes("(")){
-        mySubString = str.substring(
-            str.indexOf("(") + 1, 
-            str.lastIndexOf(")")
-        );
-        mySubString2 = str.substring(
-            0, 
-            str.lastIndexOf("(")
-        );
-    }
-    document.getElementById('groupeval').value = mySubString;
-    document.getElementById('firstname').value = mySubString2;
+    document.getElementById('firstname').value = str;
 }
 
-function elem(){
+function getValue2(selectObject) {
+    groupe = selectObject.value;
+    document.getElementById('groupeval').value = groupe;
+    elem(true);
+}
+
+function elem(call = false){
     namesearch = document.getElementById('firstname');
 
     text = '<select name="niveau" id="niveau-select" onchange="getValue(this)">';
     
 
-    var query = "PREFIX : <http://www.semanticweb.org/lucas/ontologies/2021/11/HHT_Ontology#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?name ?nomgroupe WHERE { ?x a :Area  . ?x rdfs:label ?name . ?x :isMemberOf ?groupe . ?groupe rdfs:label ?nomgroupe . FILTER regex(?name, \"" + namesearch.value + "\", \"i\") }";
+    var query = "PREFIX : <http://www.semanticweb.org/lucas/ontologies/2021/11/HHT_Ontology#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?name ?nomgroupe WHERE { ?x a :Area  . ?x rdfs:label ?name . ?x :isMemberOf ?groupe . ?groupe rdfs:label \""+groupe+"\". ?groupe  rdfs:label ?nomgroupe . FILTER regex(?name, \"" + namesearch.value + "\", \"i\") }";
     var url = 'http://localhost:7200/repositories/test?query=' + encodeURIComponent(query) + '&output=json';
+
     $.ajax({
         url: url,
         dataType: "json",
@@ -75,7 +69,7 @@ function elem(){
             $('#results').show();
             $('#raw_output').text(JSON.stringify(data, null, 3));
             for (let i = 0; i < data.results.bindings.length; i++) {
-                text += "<option value=\"" + data.results.bindings[i].name.value +" ("+data.results.bindings[i].nomgroupe.value+")"+ "\">" + data.results.bindings[i].name.value +" ("+data.results.bindings[i].nomgroupe.value+")" + "</option>";
+                text += "<option value=\"" + data.results.bindings[i].name.value + "\">" + data.results.bindings[i].name.value + "</option>";
             }
             text += '</select>';
             if(first){
@@ -87,7 +81,9 @@ function elem(){
             }
 
             document.getElementById('furtive').innerHTML = text;
-            niveau(false);
+            if(!call){
+                niveau(false);
+            }
 
 
             
@@ -97,8 +93,12 @@ function elem(){
 }
 
 function niveau(val = true){
-    text = '<select name="niveau" id="niveau-select" onchange="getValue(this)">';
-    
+    if(val){
+        text = '<select name="niveau" id="niveau-select" onchange="getValue(this)">';
+    }
+    else{
+        text = '<select name="niveau" id="niveau-select" onchange="getValue2(this)">';
+    }
 
     var query = "PREFIX : <http://www.semanticweb.org/lucas/ontologies/2021/11/HHT_Ontology#>PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>select ?name where {?x a :LevelVersion .?x rdfs:label ?name .}";
     var url = 'http://localhost:7200/repositories/test?query=' + encodeURIComponent(query) + '&output=json';
@@ -119,6 +119,9 @@ function niveau(val = true){
             else{
                 document.getElementById('furtive2').innerHTML = text;
             }
+
+            groupe = data.results.bindings[0].name.value;
+
         },
         error: function (e) { console.log("Query error"); }
     });
